d11f7fdbc53ccf6aea96bca0d2dc8eccf9a8cc44
Nzola Kiampava <nzolakiampava@gmail.com>
2024-02-20 19:01:39 +0100

    firstcommit

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Auth from './src/screens/AuthScreen';
+import PaymentScreen from './src/screens/PaymentScreen';
+import * as LocalAuthentication from "expo-local-authentication"
+import { useState, useEffect } from 'react';
 
 export default function App() {
+  const [isBiometricSupported, setIsBiometricSupported] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  //check if the hardware supports biometric authentication
+  useEffect(() => {
+    (async () => {
+      const compatible = await LocalAuthentication.hasHardwareAsync();
+      setIsBiometricSupported(compatible);
+    })();
+  });
+
+  function onAuthenticate () {
+    const auth = LocalAuthentication.authenticateAsync({
+      promptMessage: 'Authenticate',
+      fallbackLabel: 'Enter Password',
+    });
+    auth.then(result => {
+      setIsAuthenticated(result.success);
+        console.log(result);
+    }
+    );
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {
+        isAuthenticated
+        ? <PaymentScreen setIsAuthenticated={setIsAuthenticated}/>
+        : <Auth onAuthenticate={onAuthenticate} />
+      }
     </View>
   );
 }
@@ -14,7 +43,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
